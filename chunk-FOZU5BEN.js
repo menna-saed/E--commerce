5f105153ad3664824cb2c885e3d76ab056ce3fb0
menna-saed <mennasaid392@gmail.com>
2025-09-20 13:15:04 +0300

    first commit

---
+++ chunk-FOZU5BEN.js
@@ -0,0 +1,1 @@
+import{Ka as i}from"./chunk-Y5WUEYWX.js";var n=class e{transform(t,r){return t.split(" ",r).join(" ")}static \u0275fac=function(r){return new(r||e)};static \u0275pipe=i({name:"term",type:e,pure:!0})};export{n as a};
